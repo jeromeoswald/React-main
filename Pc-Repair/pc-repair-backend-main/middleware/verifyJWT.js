d11c9a5627eee3c7b45e3b3c016f974634f5dd69
Jerome Oswald <jeromeoswald7@gmail.com>
2024-06-23 14:24:54 +0530

    jwt update

--- Pc-Repair/pc-repair-backend-main/middleware/verifyJWT.js
+++ Pc-Repair/pc-repair-backend-main/middleware/verifyJWT.js
@@ -1,20 +1,20 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
-const verifyJWT = (req, res, next) => {
-	const authHeader = req.headers.authorization || req.headers.Authorization;
+module.exports = (req, res, next) => {
+  const token = req.header("Authorization")?.split(" ")[1];
+  if (!token) return res.status(401).json({ message: "Access denied" });
 
-	if (!authHeader?.startsWith('Bearer ')) {
-		return res.status(401).json({ message: 'need authentication' });
-	}
-
-	const token = authHeader.split(' ')[1];
-
-	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-		if (err) return res.status(403).json({ message: 'no content access rights' });
-		req.user = decoded.UserInfo.username;
-		req.roles = decoded.UserInfo.roles;
-		next();
-	});
+  try {
+    const verified = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+	req.user = verified.UserInfo.username;
+	req.roles = verified.UserInfo.roles;
+    next();
+  } catch (error) {
+    res.status(403).json({ message: "Invalid token" });
+  }
 };
 
 module.exports = verifyJWT;
+
+
+
