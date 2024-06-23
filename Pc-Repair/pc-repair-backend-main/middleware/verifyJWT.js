cb038bdf26fc42df931699fd1adcf86b7406c531
Jerome Oswald <jeromeoswald7@gmail.com>
2024-06-23 15:01:19 +0530

    up JWT

--- Pc-Repair/pc-repair-backend-main/middleware/verifyJWT.js
+++ Pc-Repair/pc-repair-backend-main/middleware/verifyJWT.js
@@ -4,18 +4,21 @@ const verifyJWT = (req, res, next) => {
 	const authHeader = req.headers.authorization || req.headers.Authorization;
 
 	if (!authHeader?.startsWith('Bearer ')) {
-		return res.status(401).json({ message: 'need authentication' });
+		return res.status(401).json({ message: 'need authentication', isError: true });
 	}
 
 	const token = authHeader.split(' ')[1];
 
 	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-		if (err) return res.status(403).json({ message: 'no content access rights' });
+		if (err || !decoded || !decoded.UserInfo || !decoded.UserInfo.username) {
+			return res.status(403).json({ message: 'no content access rights', isError: true });
+		}
 		req.user = decoded.UserInfo.username;
 		req.roles = decoded.UserInfo.roles;
 		next();
 	});
 };
+
 module.exports = verifyJWT;
 
 
