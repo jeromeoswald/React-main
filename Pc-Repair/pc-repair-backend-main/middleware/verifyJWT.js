327ed6bcba363bccdb8729550740a07561d6f91d
Jerome Oswald <jeromeoswald7@gmail.com>
2024-06-23 14:31:24 +0530

    upt jwt

--- Pc-Repair/pc-repair-backend-main/middleware/verifyJWT.js
+++ Pc-Repair/pc-repair-backend-main/middleware/verifyJWT.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-  const token = req.header("Authorization")?.split(" ")[1];
+  const verifyJWT = req.header("Authorization")?.split(" ")[1];
   if (!token) return res.status(401).json({ message: "Access denied" });
 
   try {
